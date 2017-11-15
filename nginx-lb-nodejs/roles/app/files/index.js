598d1b561064a68bb0cfcc7d8e3ea42f393b3912
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-11-15 16:20:29 +0100

    [nginx-lb-nodejs] Some logging on app side

--- nginx-lb-nodejs/roles/app/files/index.js
+++ nginx-lb-nodejs/roles/app/files/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 
 const getIP = require('external-ip')();
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    format: winston.format.simple(),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({filename: 'app.log'})
+    ]
+});
 
 app.get('/', (req, res) => {
+    logger.info(`New connection from ${req.connection.remoteAddress}`);
+    
     getIP((err, ip) => {
         if (err) throw err;
-        res.send(`Hello! I'm at ${ip}`);
+        
+        const message = `Hello! I'm at ${ip}`;
+        logger.info(`Sending response: "${message}"`);
+        res.send(message);
     });
 });
 
-app.listen(3000, () => console.log('App up.'));
+app.listen(3000, () => logger.info('App up.'));
